be95a067258174179f290bcc7858b05a2a1e80ca
lucasneryy <lucasnerydev@gmail.com>
2023-12-15 01:02:50 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("ola")
+let nomeHeroi = "abc"
+let xpHeroi = 1000
+let classificacao
+
+switch(true){
+    case(xpHeroi <= 1000):
+    classificacao = "Ferro"
+    break
+
+    case(xpHeroi >= 1001) && (xpHeroi <= 2000):
+    classificacao = "Bronze"
+    break
+
+    case(xpHeroi >= 2001) && (xpHeroi <= 6000):
+    classificacao = "Prata"
+    break
+
+    case(xpHeroi >= 6001) && (xpHeroi <= 7000):
+    classificacao = "Ouro"
+    break
+
+    case(xpHeroi >= 7001) && (xpHeroi <= 8000):
+    classificacao = "Platina"
+    break
+
+    case(xpHeroi >= 8001) && (xpHeroi <= 9000):
+    classificacao = "Ascendente"
+    break
+
+    case(xpHeroi >= 9001) && (xpHeroi <= 10000):
+    classificacao = "Imortal"
+    break
+
+    case(xpHeroi >= 10001):
+    classificacao = "Radiante"
+    break
+
+    default:
+    classificacao = "Indefinido"
+    break
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + classificacao)
